9afeab752f8a85dd39b8c781ce31234d962aeb04
Jasu Viding <jasu.viding@gmail.com>
2025-08-04 01:08:10 +0300

    placeholders

---
+++ config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// WIFI CONFIGURATION
+const char* WIFI_AP_SSID = "[AP-SSID]";
+const char* WIFI_AP_PASSWORD = "[AP-Password]";
+
+const char* WIFI_STA_SSID = "[STA-SSID]";
+const char* WIFI_STA_PASSWORD = "[STA-Password]";
+
+// OTA UPDATE CONFIGURATION
+const char* OTA_UPDATE_URL = "[OTA-Update-URL]";
